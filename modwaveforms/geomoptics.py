a9eea1d873df25c6832796b76093b49a34a0ed5b
ezquiaga <jose.ezquiaga@nbi.ku.dk>
2025-03-03 13:27:46 +0100

    added fold as cusp SPA waveforms

--- modwaveforms/geomoptics.py
+++ modwaveforms/geomoptics.py
@@ -53,3 +53,56 @@ def two_images_BBH(frequency_array, mass_1, mass_2, luminosity_distance, a_1, ti
     wf["cross"] = wf["cross"]*F_two_images
 
     return wf
+
+def fold_caustic(frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
+    phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, Delta_t, positive_phase, **kwargs
+    ):
+    
+    """
+    Generate the frequency domain waveform for a binary black hole system with two images
+
+    Delta_t is the time delay between the two images
+    positive_phase is the phase of the positive parity image
+    """
+
+    frequency_domain_source_model = lal_binary_black_hole
+
+    # Actually generate the waveform by calling the generator
+    wf = frequency_domain_source_model(
+        frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
+        phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, **kwargs
+    )
+
+    F_fold_two_images = positive_phase*(1. + np.exp(1j*(2*np.pi*frequency_array*Delta_t-positive_phase*np.sign(frequency_array)*np.pi/2)))
+
+    wf["plus"] = wf["plus"]*F_fold_two_images
+    wf["cross"] = wf["cross"]*F_fold_two_images
+
+    return wf
+
+def cusp_caustic(frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
+    phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, Delta_t_10, Delta_t_20, mu_rel, positive_phase, **kwargs
+    ):
+    
+    """
+    Generate the frequency domain waveform for a binary black hole system with two images
+
+    Delta_t is the time delay between the two images
+    positive_phase is the phase of the positive parity image
+    """
+
+    frequency_domain_source_model = lal_binary_black_hole
+
+    # Actually generate the waveform by calling the generator
+    wf = frequency_domain_source_model(
+        frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
+        phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, **kwargs
+    )
+
+    F_cusp_three_images = positive_phase*(1. + np.sqrt(abs(mu_rel))*np.exp(1j*(2*np.pi*frequency_array*Delta_t_10-positive_phase*np.sign(frequency_array)*np.pi/2))
+                                          + np.sqrt(1-abs(mu_rel))*np.exp(1j*(2*np.pi*frequency_array*Delta_t_20-positive_phase*np.sign(frequency_array)*np.pi/2)))
+
+    wf["plus"] = wf["plus"]*F_cusp_three_images
+    wf["cross"] = wf["cross"]*F_cusp_three_images
+
+    return wf
